ac13063fc0c774c57d4f689cc43c900e07fcc224
sagnik3788 <sagnikdas5432@gmail.com>
2025-05-29 12:03:23 +0530

    cloning/pulling feature of cd
    Signed-off-by: sagnik3788 <sagnikdas5432@gmail.com>

--- git/git.go
+++ git/git.go
@@ -1,1 +1,38 @@
 package git
+
+import (
+	"os"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+)
+
+func CloneOrPull(repoUrl, branch, path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		//if repo does not exist clone it
+		_, err := git.PlainClone(path, false, &git.CloneOptions{
+			URL:           repoUrl,
+			ReferenceName: plumbing.NewBranchReferenceName(branch), //plumbing convert our branches into git slang (like /ref/head/master)
+			SingleBranch:  true,
+			Depth:         1,
+		})
+		return err
+	}
+
+	//pull if cloned already
+	r, err := git.PlainOpen(path)
+	if err != nil {
+		return err
+	}
+
+	w, err := r.Worktree() //access of repo(commits , pr etc)
+	if err != nil {
+		return err
+	}
+
+	return w.Pull(&git.PullOptions{
+		RemoteName:    "origin",
+		ReferenceName: plumbing.NewBranchReferenceName(branch),
+	})
+
+}
